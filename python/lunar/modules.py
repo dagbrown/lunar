5a9c75d457d94b9c4a4d673ff7ed6b26df0bcb0b
Dave Brown <dagbrown@lart.ca>
2015-02-19 16:47:44 +0900

    modules.py: Added run_details.
    Instead of trying to put a full shell parser into the DETAILS file
    reader, make things a little easier by using wordexp to handle all
    of the shell-isms.

--- python/lunar/modules.py
+++ python/lunar/modules.py
@@ -3,10 +3,9 @@
 import os
 import re
 import sys
-from pkgdb import pkgdb
-from create_module_index import create_module_index
+from lunar.pkgdb import pkgdb
 from wordexp import wordexp
-from config import config
+from lunar.config import config
 
 if os.environ.has_key("MOONBASE"):
   moonbase = os.environ["MOONBASE"]
@@ -15,6 +14,7 @@ else:
 
 class Module:
   def __init__(self, name = ""):
+    """ Creates a Module object and grabs its details from its DETAILS file """
     self.name = name
     results = pkgdb.query1("""
       select location, version, updated from module_index
@@ -24,11 +24,10 @@ class Module:
       (self.location, self.version, self.updated) = results
     else:
       (self.location, self.version, self.updated) = (None, None, None)
-
-  def run_details(self):
-    pass # implement later
+    self.run_details()
 
   def _has_state(self, statename):
+    """ Check to see if a module has a specific state """
     count = pkgdb.query1("""
       select count(*) from modules_states ms
         inner join states s on ms.state_id = s.id
@@ -38,12 +37,26 @@ class Module:
     return count == 1
 
   def installed(self):
+    """ Check if module is installed """
     return self._has_state("installed") or self._has_state("held")
 
   def held(self):
+    """ Check if module has been held """
     return self._has_state("held")
 
+  def exiled(self):
+    """ Check if module has been exiled """
+    return self._has_state("exiled")
+
+  def enforced(self):
+    """ Check if module is enforced """
+    return self._has_state("enforced")
+
+  def expired(self):
+    return self.details["UPDATED"] > self.updated
+
   def installed_version(self):
+    """ Return the installed version of module """
     version = pkgdb.query1("select version from modules where package = ?", self.name )[0]
     return version
 
@@ -53,6 +66,7 @@ class Module:
       for root, dirs, files in os.walk(os.path.join(moonbase, "zlocal")):
         if self.name in dirs and os.path.exists(os.path.join(root, self.name, "DETAILS")):
           return string.replace(root, moonbase, "")
+      return self.location
     else:
       if self.location is not None:
         return self.location
@@ -62,5 +76,57 @@ class Module:
           if self.name in dirs and os.path.exists(os.path.join(root, self.name, "DETAILS")):
             return string.replace(root, moonbase, "")
 
+  def _expand_params(self,text):
+    """ Expands potential variables in a candidate string """
+    candidates = re.findall(r"\${?(\w+)", text)
+
+    if len(candidates) == 0: # nothing to do, just return the text
+      return text
+
+    # If it's been defined by module details, use that
+    # If it's been defined by lunar config, use that
+    # Finally, if it's in the environment, use tha
+    deletethese = []
+    for candidate in candidates:
+      if self.details.has_key(candidate):
+        os.environ[candidate] = self.details[candidate]
+        deletethese += [ candidate ]
+      else:
+        if config[candidate] is not None:
+          os.environ[candidate] = config[candidate]
+          deletethese += [ candidate ]
+    returnval = wordexp(text)[0]
+    for d in deletethese:
+      del os.environ[d]
+    return returnval
+
   def run_details(self):
-    pass # TODO
+    """ Loads up the values defined in the module's DETAILS file """
+    self.details={}
+    fullpath = os.path.join(moonbase,
+        self.section(),
+        self.name,
+        "DETAILS")
+    description_flag=False
+    self.description = ""
+    lines=file(fullpath, "r").read().split("\n")
+    for line in lines:
+      if re.search('^ *$',line):
+        continue
+      if re.search('^ *#', line):
+        continue
+      if description_flag and line != "EOF":
+        self.description += "\n" + line
+      if description_flag and line == "EOF":
+        self.description += "\n"
+        description_flag = False
+      if re.search("cat *\<\< *EOF$", line):
+        description_flag=True
+      try:
+        line.index("=")
+        variable, value = line.split("=", 1)
+        variable=wordexp(variable)[0]
+        value = self._expand_params(value)
+        self.details[variable] = value
+      except ValueError:
+        pass
